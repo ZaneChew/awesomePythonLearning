46fb8e107ee4ebf1869a716b82736a2714728df2
zanezeyha <zanezeyha@outlook.com>
2025-08-08 12:10:02 +0800

    the using way of tuple

---
+++ 03tuple.py
@@ -0,0 +1,29 @@
+tu = (1, "太⽩白", "李李⽩白", "太⿊黑", "怎么⿊黑")
+
+print(tu[0:2])
+print(tu[:2])
+print(tu[2:])
+print(tu[0:-1:2])
+print(tu[1::2])
+print(tu[::2])
+print(tu[::-1])
+
+a=tu.count(1)
+b=tu.index("太⽩白")
+c=tu.index(1)
+print(a)
+print(b)
+print(c)
+
+
+
+for num in range(10):
+    print(num)
+
+for num in range(1,10,2):
+    print(num)
+for num in range(1,10):
+    print(num)
+
+
+
